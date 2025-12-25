724db45a29852b02863d54110a915f70505a632f
sparks685 <sparkyv7@hotmail.com>
2025-12-25 05:18:38 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Limitless Forms 🚀 MVP is live");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
